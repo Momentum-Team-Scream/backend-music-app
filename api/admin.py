100910dadb2b71e585e04b3fcd42c9fc49728ae0
Middleway13 <middlewayonlinemedia@gmail.com>
2021-10-04 15:47:23 -0400

    Created Notes Model and Admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import User, Lesson, Note
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['pk',]
+    
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'title', 'body', 'is_shared', 'created_at',]
--- api/models.py
+++ api/models.py
@@ -5,3 +5,15 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+class Note(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    lesson = models.ForeignKey(
+        to="Lesson", on_delete=models.CASCADE, related_name="lesson_note")
+    is_shared = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return f"{self.title}"
