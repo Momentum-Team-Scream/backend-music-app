cb2945c1a6c63fe5eb8a93a514c1bf85489a6635
Em Sparr <amberem@gmail.com>
2021-10-04 21:26:04 -0400

    Adds User model fields, migration

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator
 from django.contrib.auth.models import AbstractUser
 
 
@@ -6,6 +7,12 @@ class User(AbstractUser):
     USER_CREATE_PASSWORD_RETYPE = True
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['first_name', 'last_name', 'email', 'emergency_contact_name', 'emergency_contact_phone']
+
+    emergency_contact_name = models.CharField(max_length=255)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    emergency_contact_phone =  models.CharField(validators=[phone_regex], max_length=17) 
+    
+    is_instructor = models.BooleanField(default=False)
 
     def __repr__(self):
         return f"<User username={self.username}>"
