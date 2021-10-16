8643828b77b61b7eaad0fb990f11607448b1c32c
Em Sparr <amberem@gmail.com>
2021-10-16 16:01:41 -0400

    Updated AWS settings in hopes of making uploaded docs public.

--- music/settings.py
+++ music/settings.py
@@ -174,6 +174,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
+AWS_DEFAULT_ACL = 'public-read'
 AWS_PRELOAD_METADATA = True
 AWS_QUERYSTRING_AUTH = True
 
